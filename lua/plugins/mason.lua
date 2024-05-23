8ce7d837838a703ceb36522296fb4fd7d7bf38a4
phenpessoa <phenpessoa@gmail.com>
2024-05-23 15:42:40 -0300

    mason enhancements

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,25 +1,62 @@
+local ensure_installed = {
+    "typescript-language-server",
+    "prettier",
+
+    "gopls",
+    "golangci-lint",
+    "golangci-lint-langserver",
+    "golines",
+    "goimports-reviser",
+
+    "rust-analyzer",
+
+    "html-lsp",
+    "htmx-lsp",
+    "tailwindcss-language-server",
+
+    "lua-language-server",
+}
+
+local options = {
+    ensure_installed = ensure_installed, -- not an option from mason.nvim
+
+    PATH = "skip",
+
+    ui = {
+        icons = {
+          package_pending = " ",
+          package_installed = "󰄳 ",
+          package_uninstalled = " 󰚌",
+        },
+
+        keymaps = {
+          toggle_server_expand = "<CR>",
+          install_server = "i",
+          update_server = "u",
+          check_server_version = "c",
+          update_all_servers = "U",
+          check_outdated_servers = "C",
+          uninstall_server = "X",
+          cancel_installation = "<C-c>",
+        },
+    },
+
+    max_concurrent_installers = 10,
+}
+
 return {
     {
         "williamboman/mason.nvim",
-        opts = {
-            ensure_installed = {
-                "typescript-language-server",
-                "prettier",
+        cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
+        opts = options,
+        config = function(_, opts)
+            require("mason").setup(opts)
 
-                "gopls",
-                "golangci-lint",
-                "golangci-lint-langserver",
-                "golines",
-                "goimports-reviser",
+            vim.api.nvim_create_user_command("MasonInstallAll", function()
+                vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
+            end, {})
 
-                "rust-analyzer",
-
-                "html-lsp",
-                "htmx-lsp",
-                "tailwindcss-language-server",
-
-                "lua-language-server",
-            },
-        },
+            vim.g.mason_binaries_list = opts.ensure_installed
+        end,
     },
 }
