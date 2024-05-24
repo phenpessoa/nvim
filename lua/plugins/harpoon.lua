499271df7dcba765d7eb04ef39deeee857dea1c3
phenpessoa <phenpessoa@gmail.com>
2024-05-24 16:04:08 -0300

    simplify harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -4,66 +4,15 @@ return {
         branch = "harpoon2",
         dependencies = {
             "nvim-lua/plenary.nvim",
-            "nvim-telescope/telescope.nvim",
         },
         event = { "VeryLazy" },
         config = function()
             local harpoon = require "harpoon"
             harpoon:setup()
+            require("telescope").load_extension("harpoon")
         end,
         keys = function()
             local harpoon = require "harpoon"
-            local conf = require("telescope.config").values
-            local action_state = require('telescope.actions.state')
-            local transform_mod = require('telescope.actions.mt').transform_mod
-
-            local function get_harpoon_file_paths()
-                local file_paths = {}
-                for _, item in ipairs(harpoon:list().items) do
-                    table.insert(file_paths, item.value)
-                end
-                return file_paths
-            end
-
-            local actions = transform_mod({
-                remove_item_under_cursor = function(prompt_bufnr)
-                    local current_picker = action_state.get_current_picker(prompt_bufnr)
-                    local current_entry = action_state.get_selected_entry()
-
-                    if current_entry then
-                        local item = current_entry.value
-                        harpoon:list():remove({ value = item })
-                        local finder = require("telescope.finders").new_table({
-                            results = get_harpoon_file_paths(),
-                            entry_maker = current_picker.finder.entry_maker,
-                        })
-
-                        current_picker:refresh(finder, { reset_prompt = false })
-                    end
-                end
-            })
-
-
-            require("telescope").setup({
-                defaults = {
-                    mappings = {
-                        i = {
-                            ["<C-d>"] = actions.remove_item_under_cursor,
-                        },
-                    },
-                }
-            })
-
-            local function toggle_telescope()
-                require("telescope.pickers").new({}, {
-                    prompt_title = "Harpoon",
-                    finder = require("telescope.finders").new_table({
-                        results = get_harpoon_file_paths(),
-                    }),
-                    previewer = conf.file_previewer({}),
-                    sorter = conf.generic_sorter({}),
-                }):find()
-            end
 
             return {
                 {
@@ -76,9 +25,16 @@ return {
                 {
                     "<leader>ho",
                     function()
-                        toggle_telescope()
+                        harpoon.ui:toggle_quick_menu(harpoon:list())
                     end,
                     desc = "Toggle harpoon's quick menu",
+                },
+                {
+                    "<leader>hc",
+                    function()
+                        harpoon:list():clear()
+                    end,
+                    desc = "Clear harpoon list",
                 },
                 {
                     "<Tab>",
