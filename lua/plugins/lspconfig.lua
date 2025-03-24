d94d8f92713840b6ebbd224f7355beab1d109983
phenpessoa <phenpessoa@gmail.com>
2025-03-24 19:17:58 -0300

    update for golangci-lint 2.0

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -205,29 +205,22 @@ return {
 				},
 			})
 
-			local configs = require("lspconfig/configs")
-			if not configs.golangcilsp then
-				configs.golangcilsp = {
-					default_config = {
-						cmd = { "golangci-lint-langserver" },
-						root_dir = util.root_pattern(".git", "go.mod"),
-						init_options = {
-							command = {
-								"golangci-lint",
-								"run",
-								"--enable-all",
-								"--disable",
-								"lll",
-								"--out-format",
-								"json",
-								"--issues-exit-code=1",
-							},
-						},
-					},
-				}
-			end
 			lspconfig.golangci_lint_ls.setup({
 				filetypes = { "go", "gomod" },
+				cmd = { "golangci-lint-langserver" },
+				root_dir = util.root_pattern(".git", "go.mod"),
+				init_options = {
+					command = {
+						"golangci-lint",
+						"run",
+						"--output.text.path",
+						"/dev/null",
+						"--output.json.path",
+						"stdout",
+						"--issues-exit-code=1",
+						"--show-stats=false",
+					},
+				},
 			})
 
 			lspconfig.intelephense.setup({
