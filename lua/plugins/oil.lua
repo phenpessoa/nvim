ac46d4970989a273c535dcfd698bee22ee29b262
phenpessoa <phenpessoa@gmail.com>
2026-08-16 14:07:21 -0300

    show files permissions and size

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,11 +3,26 @@ return {
 		"stevearc/oil.nvim",
 		---@module 'oil'
 		---@type oil.SetupOpts
-		opts = {},
-		-- Optional dependencies
+		opts = {
+			columns = {
+				"icon",
+				"permissions",
+				"size",
+				{ "spacer", highlight = "NonText" },
+			},
+		},
+		config = function(_, opts)
+			require("oil.columns").register("spacer", {
+				render = function()
+					return "│"
+				end,
+				parse = function(line)
+					return line:match("^([│]*)%s*(.*)$")
+				end,
+			})
+			require("oil").setup(opts)
+		end,
 		dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-		-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
 		lazy = false,
 	},
 }
